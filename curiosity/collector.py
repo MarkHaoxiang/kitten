Further bug fixes: TD error weighting problem for ddpg, reconsidered maximum priority calculation, added beta annealing

curiosity/collector.py
@@ -62,7 +62,7 @@ class GymCollector(DataCollector):
         self.device = device
         super().__init__(policy, env, memory)
 
-    def collect(self, n: int, *args, **kwargs) -> List:
+    def collect(self, n: int, early_start: bool=False, *args, **kwargs) -> List:
         with torch.no_grad():
             result = []
             obs = self.obs
@@ -81,7 +81,7 @@ class GymCollector(DataCollector):
                 transition_tuple = (obs, action, reward, n_obs, terminated)
                 result.append(transition_tuple)
                 if not self.memory is None:
-                    self.memory.append(transition_tuple)
+                    self.memory.append(transition_tuple, update=not early_start)
                 obs = n_obs
             self.obs = obs
             return result
@@ -89,7 +89,7 @@ class GymCollector(DataCollector):
     def early_start(self, n: int) -> List:
         policy = self.policy
         self.set_policy(Policy(lambda _: self.env.action_space.sample(), transform_obs=False))
-        result = self.collect(n)
+        result = self.collect(n, early_start=True)
         self.policy = policy
         return result
 

curiosity/experience.py
@@ -32,7 +32,7 @@ class ReplayBuffer:
         self._append_index = 0 # Position of next tensor to be inserted
         self._full = False # Bookmark to check if storage has looped around
 
-    def append(self, inputs: tuple[Tensor]) -> int:
+    def append(self, inputs: tuple[Tensor], **kwargs) -> int:
         """ Add a transition experience to the buffer
 
         Args:
@@ -125,29 +125,36 @@ class PrioritizedReplayBuffer(ReplayBuffer):
                  shape: Sequence[int],
                  epsilon: float = 0.1,
                  alpha: float = 0.6,
-                 beta: float = 0,
+                 beta_0: float = 0.4,
                  dtype: Optional[Union[torch.dtype, Sequence[torch.dtype]]] = None,
+                 beta_annealing_steps: int = 10000,
                  device: str = "cpu") -> None:
         super().__init__(capacity, shape, device, dtype)
         
         self.epsilon = epsilon
         self.alpha = alpha
-        self.beta = beta
+        self.beta_0 = beta_0
+        self.beta_annealing_steps = beta_annealing_steps
+        self.timestep = 0
         self.sum_tree_offset = (1 << (capacity-1).bit_length()) - 1
         self.sum_tree = np.zeros(self.sum_tree_offset+capacity)
         self.error_fn = error_fn
+        self._maximum_priority = 1.0
 
-    def append(self, inputs: tuple[Tensor]) -> None:
+    def append(self, inputs: tuple[Tensor], update: bool= True) -> None:
+        """ Add transitions to the replay buffer
+
+        Args:
+            inputs (tuple[Tensor]): Transitions
+            update (bool, optional): Update annealing step. Defaults to True.
+        """
         initial_append_index = self._append_index
         n_insert = super().append(inputs)
-
+        if update:
+            self.timestep = min(self.beta_annealing_steps, self.timestep + n_insert)
         for i in range(initial_append_index, initial_append_index + n_insert):
             j = i % self.capacity
-                # Insert maximum priority
-            priority = self._calculate_priority(self.error_fn(self._fetch_storage([j])))
-            #priority = np.max([priority[0], np.max(self.sum_tree[self.sum_tree_offset:])])
-            #priority = np.max([100, np.max(self.sum_tree[self.sum_tree_offset:])])
-            self._sift_up(priority, j)
+            self._sift_up(self._maximum_priority, j)
 
     def sample(self, n: int, continuous: bool = False) -> tuple[Tensor]:
         # Invariant checks
@@ -165,9 +172,13 @@ class PrioritizedReplayBuffer(ReplayBuffer):
         selection = self._get(selection)
 
         # Calculate Weightings
-        sampled_priorities = self.sum_tree[selection + self.sum_tree_offset]
-        sampled_probabilities = sampled_priorities / self.sum_tree[0]
-        w = 1 / (len(self) * sampled_probabilities)**self.beta
+            # Annealing
+        beta = self.beta_0 + (self.timestep / self.beta_annealing_steps) * (1-self.beta_0)
+            # Probabities = Priority / Total Priority
+        sampled_probabilities = (self.sum_tree[selection + self.sum_tree_offset]) / self.sum_tree[0]
+            # Importance sampling
+        w = (len(self) * sampled_probabilities)**(-beta)
+            # Numerical stability
         w = w / np.max(w)
         w = torch.from_numpy(w).to(self.device)
 
@@ -175,6 +186,7 @@ class PrioritizedReplayBuffer(ReplayBuffer):
         # Update errors 
         priorities = self._calculate_priority(self.error_fn(results))
         for i, j in enumerate(selection):
+            self._maximum_priority = max(self._maximum_priority, priorities[i])
             self._sift_up(priorities[i], j)
 
         # Transform indices to result tuples
@@ -213,7 +225,8 @@ def build_replay_buffer(env: Env,
                         capacity: int = 10000,
                         type = "experience_replay",
                         error_fn: Optional[Callable] = None,
-                        device: str = "cpu") -> ReplayBuffer:
+                        device: str = "cpu",
+                        **kwargs) -> ReplayBuffer:
     """ Creates a replay buffer for env
 
     Args:
@@ -230,7 +243,8 @@ def build_replay_buffer(env: Env,
             capacity=capacity,
             shape=(env.observation_space.shape, env.action_space.shape, (), env.observation_space.shape, ()),
             dtype=(torch.float32, torch.int if discrete else torch.float32, torch.float32, torch.float32, torch.bool),
-            device=device
+            device=device,
+            **kwargs
         )
     elif type == "prioritized_experience_replay":
         if error_fn is None:
@@ -240,6 +254,7 @@ def build_replay_buffer(env: Env,
             capacity=capacity,
             shape=(env.observation_space.shape, env.action_space.shape, (), env.observation_space.shape, ()),
             dtype=(torch.float32, torch.int if discrete else torch.float32, torch.float32, torch.float32, torch.bool),
-            device=device
+            device=device,
+            **kwargs
         )
     raise NotImplementedError("Replay buffer type not supported")

curiosity/rl.py
@@ -51,10 +51,8 @@ class DeepDeterministicPolicyGradient:
         Returns:
             float: critic loss
         """
-        x, y = self.error(s_0, a, r, s_1, d)
- 
-        loss = (y-x)**2
-        loss = torch.mean(torch.mean(loss, dim=list(range(1, len(loss.shape)))) * weights)
+        td_error = self.td_error(s_0, a, r, s_1, d) * weights
+        loss = torch.mean(td_error**2)
         loss_value = loss.item()
         self._optim_critic.zero_grad()
         loss.backward()
@@ -64,14 +62,14 @@ class DeepDeterministicPolicyGradient:
 
         return loss_value
     
-    def error(self, s_0: Tensor, a: Tensor, r: Tensor, s_1: Tensor, d: Tensor) -> float:
+    def td_error(self, s_0: Tensor, a: Tensor, r: Tensor, s_1: Tensor, d: Tensor) -> float:
         """ Returns TD difference for a transition
         """
         x = self.critic(torch.cat((s_0, a), 1)).squeeze()
         with torch.no_grad():
             target_max = (~d) * self.critic.target(torch.cat((s_1, self.actor.target(s_1)), 1)).squeeze()
             y = (r + target_max * self._gamma)
-        return x, y
+        return y-x
 
     def _actor_update(self, s_0: Tensor, weights: Tensor) -> float:
         """ Runs a critic update

scripts/ddpg.py
@@ -107,12 +107,11 @@ def train(config: Dict = {},
     )
 
     # Initialise Data Pipeline
-    def td_error(transition):
-        x,y = ddpg.error(*transition)
-        return torch.abs(x-y).detach().cpu().numpy()
     memory = build_replay_buffer(
         env, capacity=replay_buffer_capacity, device=DEVICE,
-        type="prioritized_experience_replay", error_fn=td_error
+        type="prioritized_experience_replay",
+        error_fn=lambda x: torch.abs(ddpg.td_error(*x)).detach().cpu().numpy(),
+        beta_annealing_steps = total_frames
     )
     collector = build_collector(policy, env, memory, device=DEVICE)
 