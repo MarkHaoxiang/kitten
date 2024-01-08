Working disagreement (may require reward normalisation update). Bug fixes

curiosity/intrinsic/disagreement.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 from curiosity.experience import Transition
 from curiosity.intrinsic.intrinsic import IntrinsicReward
 
-class IntrinsicCuriosityModule(IntrinsicReward):
+class Disagreement(IntrinsicReward):
     """ Self-Supervised Exploration via Disagreement
 
     Pathak et al. https://arxiv.org/pdf/1906.04161.pdf
@@ -35,11 +35,20 @@ class IntrinsicCuriosityModule(IntrinsicReward):
     def _forward_models(self, phi_0: torch.Tensor, a: torch.Tensor) -> torch.Tensor:
         """ Predicts next encodings
         """
-        batch_size = phi_0.shape[:-1]
-        return torch.stack([h(torch.cat((phi_0, a), len(batch_size)))for h in self.forward_heads])
+        return torch.stack([h(torch.cat((phi_0, a), -1)) for h in self.forward_heads])
 
-    def _featurise(self, s):
-        if self.feature_net is None:
+    def _featurise(self, s: Tensor) -> Tensor:
+        """ Encodes into a latent space
+
+        Ideally, the latent space should be easy to learn from and relevant to the goal
+
+        Args:
+            s (Tensor): Observations
+
+        Returns:
+            Tensor: Encoded states
+        """
+        if not self.feature_net is None:
             return self.feature_net(s)
         return s 
 
@@ -57,5 +66,5 @@ class IntrinsicCuriosityModule(IntrinsicReward):
     def _reward(self, batch: Transition):
         phi_0 = self._featurise(batch.s_0)
         pred_phi_1 = self._forward_models(phi_0, batch.a)
-        r_i = torch.mean(torch.var(pred_phi_1, dim=0))
+        r_i = torch.mean(torch.var(pred_phi_1, dim=0), dim=-1)
         return r_i

curiosity/intrinsic/rnd.py
@@ -47,7 +47,7 @@ class RandomNetworkDistillation(IntrinsicReward, nn.Module):
 
         random_embedding = self.target_net(s_1)
         predicted_embedding = self.predictor_net(s_1)
-        r_i = ((random_embedding - predicted_embedding)**2).sum(-1) / 2
+        r_i = ((random_embedding - predicted_embedding)**2).mean(-1)
         return r_i
 
     def update(self, batch: Transition, weights: Tensor, step: int):

curiosity/util/util.py
@@ -13,6 +13,7 @@ from curiosity.nn import ClassicalBoxActor
 from curiosity.intrinsic.intrinsic import IntrinsicReward, NoIntrinsicReward
 from curiosity.intrinsic.icm import IntrinsicCuriosityModule
 from curiosity.intrinsic.rnd import RandomNetworkDistillation
+from curiosity.intrinsic.disagreement import Disagreement
 from curiosity.rl.ddpg import DeepDeterministicPolicyGradient
 from curiosity.rl.td3 import TwinDelayedDeepDeterministicPolicyGradient
 
@@ -137,11 +138,13 @@ def build_critic(env: Env, features: int) -> nn.Module:
     return result
 
 
-def build_intrinsic(env, intrinsic_configuration, device: str) -> IntrinsicReward:
+def build_intrinsic(env, intrinsic_configuration, device: str = "cpu") -> IntrinsicReward:
     if intrinsic_configuration.type == "icm":
         return build_icm(env=env, device=device, **intrinsic_configuration)
     elif intrinsic_configuration.type =="rnd":
         return build_rnd(env=env, device=device, **intrinsic_configuration)
+    elif intrinsic_configuration.type =="disagreement":
+        return build_disagreement(env=env, device=device, **intrinsic_configuration)
     return NoIntrinsicReward()
 
 def build_icm(env: Env, encoding_size: int, device: str, clip_grad_norm: Optional[float] = 1, **kwargs):
@@ -176,7 +179,7 @@ def build_icm(env: Env, encoding_size: int, device: str, clip_grad_norm: Optiona
     result =  IntrinsicCuriosityModule(feature_net, forward_head, inverse_head, discrete_action_space=False, **kwargs)
     return result
 
-def build_rnd(env: Env, encoding_size: int, device:str, lr: float = 1e-3, **kwargs):
+def build_rnd(env: Env, encoding_size: int, lr: float = 1e-3, device: str = 'cpu', **kwargs):
     obs_size = env.observation_space.shape[0]
     target_net = nn.Sequential(
         nn.Linear(in_features=obs_size, out_features=encoding_size),
@@ -191,6 +194,20 @@ def build_rnd(env: Env, encoding_size: int, device:str, lr: float = 1e-3, **kwar
         nn.LeakyReLU()
     ).to(device=device)
     return RandomNetworkDistillation(target_net, predictor_net, lr)
+
+def build_disagreement(env: Env, encoding_size: int, lr: float = 1e-3, device: str = 'cpu', **kwargs):
+    obs_size = env.observation_space.shape[0]
+    action_size = env.action_space.shape[0]
+    # TODO: How to integrate different featurisers
+    def build_forward_head():
+        return nn.Sequential(
+            nn.Linear(in_features=obs_size + action_size, out_features=encoding_size),
+            nn.LeakyReLU(),
+            nn.Linear(in_features=encoding_size, out_features=encoding_size),
+            nn.LeakyReLU(),
+            nn.Linear(in_features=encoding_size, out_features=obs_size)
+        ).to(device=device)
+    return Disagreement(build_forward_head, feature_net= None, **kwargs)
 
 def global_seed(seed: int, *envs):
     """ Utility to help set determinism