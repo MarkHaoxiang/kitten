2d2ef4a9994419897a792781d3ff90c1c77ef9fd
MarkHaoxiang <mark.haoxiang@gmail.com>
2024-02-27 15:07:22 +0000

    Bug revert

--- kitten/policy.py
+++ kitten/policy.py
@@ -3,19 +3,17 @@ import random
 
 import gymnasium as gym
 from gymnasium.spaces import Space
+import pink
 import numpy as np
 import torch
 from torch import Tensor
 
-
 class Policy:
-    def __init__(
-        self,
-        fn: Callable[[Union[Tensor, np.ndarray]], Tensor],
-        transform_obs: bool = True,
-        device="cpu",
-    ):
-        """Policy API to pass into data collection pipeline
+    def __init__(self,
+                 fn: Callable[[Union[Tensor, np.ndarray]], Tensor],
+                 transform_obs: bool = True,
+                 device="cpu"):
+        """ Policy API to pass into data collection pipeline
 
         Args:
             fn (Callable): Policy taking observations and returning actions.
@@ -28,7 +26,7 @@ class Policy:
         self.device = device
 
     def __call__(self, obs: Union[Tensor, np.ndarray], *args, **kwargs) -> Any:
-        """Actions from observations
+        """ Actions from observations
 
         Args:
             inputs (Any): observations.
@@ -37,39 +35,43 @@ class Policy:
             Any: actions.
         """
         if self._transform_obs:
-            obs = torch.tensor(obs, device=self.device, dtype=torch.float32)
-
+            obs = torch.tensor(
+                obs,
+                device=self.device,
+                dtype=torch.float32
+            )
+        
         return self.fn(obs)
 
     def reset(self) -> None:
-        """Called on episode reset"""
+        """ Called on episode reset
+        """
         pass
 
     def enable_evaluation(self) -> None:
-        """Disable exploration for evaluation"""
+        """Disable exploration for evaluation
+        """
         self._evaluate = True
 
     def disable_evaluation(self) -> None:
-        """Enable exploration for training"""
+        """Enable exploration for training
+        """
         self._evaluate = False
 
     @property
     def evaluate(self) -> bool:
         return self._evaluate
-
+    
     @property
     def train(self) -> bool:
         return not self._evaluate
 
-
 class EpsilonGreedyPolicy(Policy):
-    def __init__(
-        self,
-        fn: Callable[[Union[Tensor, np.ndarray]], Tensor],
-        action_space: Space,
-        device: str = "cpu",
-    ):
-        """Epsilon Greedy Exploration
+    def __init__(self,
+                 fn: Callable[[Union[Tensor, np.ndarray]], Tensor],
+                 action_space: Space,
+                 device: str = "cpu"):
+        """ Epsilon Greedy Exploration
 
         Randomly sample from action_space randomly.
 
@@ -87,19 +89,16 @@ class EpsilonGreedyPolicy(Policy):
                 return self._action_space.sample()
         return x
 
-
 class ColoredNoisePolicy(Policy):
-    def __init__(
-        self,
-        fn: Callable[[Union[Tensor, np.ndarray]], Tensor],
-        action_space: Space,
-        episode_length: Optional[int],
-        scale: Union[float, np.ndarray, torch.Tensor] = 1,
-        beta: float = 1,
-        rng=None,
-        device: str = "cpu",
-    ):
-        """Pink Noise Exploration
+    def __init__(self,
+                 fn: Callable[[Union[Tensor, np.ndarray]], Tensor],
+                 action_space: Space,
+                 episode_length: Optional[int],
+                 scale: Union[float, np.ndarray, torch.Tensor] = 1,
+                 beta: float = 1,
+                 rng = None,
+                 device: str = "cpu"):
+        """ Pink Noise Exploration
 
         Eberhard et al.
         Pink Noise Is All You Need: Colored Noise Exploration in Deep Reinforcement Learning
@@ -113,7 +112,7 @@ class ColoredNoisePolicy(Policy):
             device (str, optional): Training device. Defaults to "cpu".
         """
         super().__init__(fn, device=device)
-        # Calculate shape
+            # Calculate shape
         if isinstance(action_space, gym.spaces.Box):
             size = action_space.shape
         elif isinstance(action_space, gym.spaces.Discrete):
@@ -123,13 +122,16 @@ class ColoredNoisePolicy(Policy):
         if episode_length is None:
             episode_length = 1000
         self._noise = pink.ColoredNoiseProcess(
-            beta=beta, size=(*size, episode_length), scale=1, rng=rng
+            beta=beta,
+            size=(*size, episode_length),
+            scale=1,
+            rng = rng
         )
         self._device = device
         self.scale = torch.tensor(scale, device=self._device)
 
     def __call__(self, obs: Union[Tensor, np.ndarray]) -> Any:
-        action = super().__call__(obs)
+        action =  super().__call__(obs)
         if self.train:
             noise = self._noise.sample(1)
             if isinstance(action, torch.Tensor):
